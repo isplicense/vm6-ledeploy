f807679965fbcf5db0f252577b1c94e0860cfe60
Maxim Shpakov <my@isplicense.com>
2024-04-11 22:03:27 +0000

    Added API retry https://docs.ispsystem.ru/vmmanager-admin/razrabotchiku/rabota-s-api#id-%D0%A0%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%81API-%D0%9A%D0%B0%D0%BA%D0%BE%D0%B1%D1%80%D0%B0%D0%B1%D0%B0%D1%82%D1%8B%D0%B2%D0%B0%D1%82%D1%8C%D0%BA%D0%BE%D0%B4503 Added x-xsrf-token for nginxctl/v1/ssl api call. More robust error handling and reporting.

--- vm6-le.sh
+++ vm6-le.sh
@@ -2,19 +2,49 @@
 # Renew hook script to deploy Let's Encrypt cert to VM6 through API.
 #
 # USER_ID from vmmanager with admin rights
-USER_ID=1
+USER_ID="1"
+CERT_PATH="$RENEWED_LINEAGE" #/etc/letsencrypt/live/domain.com
 
-KEY=$(docker exec vm_box curl -s -k -X POST http://input:1500/auth/v4/user/$USER_ID/key -d '{}' -H 'internal-auth:on' | jq '{"key": .key}')
-[[ "$KEY" == "null" ]] && exit 1
+RETRY=0
+while [[ true ]]
+do
+    KEY_RESULT=$(docker exec vm_box curl -s -k -X POST http://input:1500/auth/v4/user/$USER_ID/key -d '{}' -H 'internal-auth:on')
+    KEY=$(jq -r .key <<< $KEY_RESULT)
 
-SESSION=$(curl -s -k https://localhost/auth/v4/public/key -H "isp-box-instance: true" -d "$KEY" -k | jq -s -r .[].token)
-[[ "$SESSION" == "null" ]] && exit 1
+    [[ $? -eq 0 ]] && [[ "$KEY" != "null" && ! -z "$KEY" ]] && break
 
-PAYLOAD=$(jq --arg certificate "$(<$RENEWED_LINEAGE/cert.pem)" --arg ca_bundle "$(<$RENEWED_LINEAGE/chain.pem)" --arg private_key "$(<$RENEWED_LINEAGE/privkey.pem)" \
+    ((RETRY++)) && ((RETRY==10)) && echo "API Error: auth/v4/user/$USER_ID/key" && exit 1
+    sleep 1
+done
+
+RETRY=0
+while [[ true ]]
+do
+    SESSION_RESULT=$(curl -s -k https://localhost/auth/v4/public/key -H "isp-box-instance: true" -d "{\"key\": \"$KEY\"}")
+    SESSION=$(jq -r .token <<< $SESSION_RESULT)
+
+    [[ $? -eq 0 ]] && [[ "$SESSION" != "null" && ! -z "$SESSION" ]] && break
+
+    ((RETRY++)) && ((RETRY==10)) && echo "API Error: auth/v4/public/key" && exit 1
+    sleep 1
+done
+
+[[ ! -f "$CERT_PATH/cert.pem" || ! -f "$CERT_PATH/chain.pem" || ! -f "$CERT_PATH/privkey.pem" ]] && echo "Error: Certificates not found" && exit 1
+
+PAYLOAD=$(jq --arg certificate "$(<$CERT_PATH/cert.pem)" --arg ca_bundle "$(<$CERT_PATH/chain.pem)" --arg private_key "$(<$CERT_PATH/privkey.pem)" \
              '{"name":"ssl_cert","certificate":$certificate,"ca_bundle":$ca_bundle,"private_key":$private_key}' <<< '{}')
 
-RESULT=$(curl --write-out %{http_code} "https://localhost/nginxctl/v1/ssl" -H "Cookie: ses6=$SESSION" -d "$PAYLOAD" -s -k)
+RETRY=0
+while [[ true ]]
+do
+    PAYLOAD_RESULT=$(curl --write-out %{http_code} -s -k "https://localhost/nginxctl/v1/ssl" -H "Cookie: ses6=$SESSION" -H "x-xsrf-token: $SESSION" -d "$PAYLOAD")
 
-[[ "$RESULT" != "200" ]] && exit 1
+    [[ "$PAYLOAD_RESULT" == "200" ]] && break
+
+    ((RETRY++)) && ((RETRY==10)) && echo "API Error: nginxctl/v1/ssl" && exit 1
+    sleep 1
+done
+
+echo "Success."
 
 exit 0
